faf1ac36f42e7509f3104f18cc1d48c3395997ae
Andy <931663592@163.com>
2018-12-06 11:03:06 +0800

    //

--- app/src/main/java/com/example/andy/Interface/HttpCallbackListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.andy.Interface;
-
-public interface HttpCallbackListener {
-
-    void onFinish(String response);
-
-    void onError(Exception e);
-
-}
--- app/src/main/java/com/example/andy/Interface/OnResponseListner.java
+++
@@ -1,6 +0,0 @@
-package com.example.andy.Interface;
-
-public interface OnResponseListner {
-    void onSucess(String response);
-    void onError(String error);
-}
