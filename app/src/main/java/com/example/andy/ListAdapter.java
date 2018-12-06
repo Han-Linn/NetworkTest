f56533ab4cd01d69048f3f99bbced54629f2f182
Andy <931663592@163.com>
2018-12-06 18:00:48 +0800

    //

---
+++ app/src/main/java/com/example/andy/ListAdapter.java
@@ -0,0 +1,77 @@
+package com.example.andy;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.andy.JavaBean.App;
+import com.example.networktest.R;
+
+import java.util.List;
+import java.util.Map;
+
+public class ListAdapter extends BaseAdapter {
+    private Context context;
+    private List<App> list;
+
+    public ListAdapter(Context context, List<App> dataList) {
+        this.context = context;
+        this.list = dataList;
+    }
+
+    @Override//获取适配器中数据集中数据的条目数
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override//获取数据集中与指定索引对应的数据项(不用)
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override//获取数据集中指定索引对应的项的id(不用)
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+    class ViewHolder {
+        private TextView Name_Theme, DateTime1,DateTime2,Name_AppointMan;
+    }
+
+    @Override//重点
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewholder = null;
+        if (convertView == null) {
+            viewholder = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.layout_listview, null);
+            viewholder.Name_Theme = convertView.findViewById(R.id.name_theme);
+            viewholder.DateTime1 = convertView.findViewById(R.id.datetime1);
+            viewholder.DateTime2 = convertView.findViewById(R.id.datetime2);
+            viewholder.Name_AppointMan = convertView.findViewById(R.id.name_appointMan);
+            convertView.setTag(viewholder);
+        } else {
+            viewholder = (ViewHolder) convertView.getTag();
+        }
+
+//        String Name = list.get(position).get("addr").toString();
+//        String DateTime1 = list.get(position).get("startTime").toString();
+//        String DateTime2 = list.get(position).get("endTime").toString();
+//        String Name_AppointMan = list.get(position).get("appointMan").toString();
+
+        String Name = list.get(position).getAddr();
+        String DateTime1 = list.get(position).getStartTime();
+        String DateTime2 = list.get(position).getEndTime();
+        String Name_AppointMan = list.get(position).getAppointMan();
+
+        viewholder.Name_Theme.setText(Name);
+        viewholder.DateTime1.setText(DateTime1);
+        viewholder.DateTime2.setText(DateTime2);
+        viewholder.Name_AppointMan.setText(Name_AppointMan);
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/networktest/App.java
@@ -0,0 +1,78 @@
+package com.example.networktest;
+
+public class App {
+
+    private String addr;
+
+    private String appointMan;
+
+    private String createTime;
+
+    private String endTime;
+
+    private String reason;
+
+    private String startTime;
+
+    public App(App app) {
+        this.addr=app.getAddr();
+        this.appointMan=app.getAppointMan();
+        this.createTime=app.getCreateTime();
+        this.endTime=app.getEndTime();
+        this.reason=app.getReason();
+        this.startTime=app.getStartTime();
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getAppointMan() {
+        return appointMan;
+    }
+
+    public void setAppointMan(String appointMan) {
+        this.appointMan = appointMan;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+
+
+
+
+}
