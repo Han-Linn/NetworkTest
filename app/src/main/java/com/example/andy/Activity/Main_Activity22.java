9013295d6950616ede355e333bad9a3bbc5649e8
Andy <931663592@163.com>
2018-12-12 17:14:03 +0800

    //

--- app/src/main/java/com/example/andy/Activity/Main_Activity22.java
+++ app/src/main/java/com/example/andy/Activity/Main_Activity22.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 import com.example.networktest.R;
 
-public class Main_Activity extends AppCompatActivity {
+public class Main_Activity22 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
